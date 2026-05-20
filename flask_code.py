8786e7b99cda62dde183008be689b0e757a1f9ff
saimaheshgudla <saimaheshgudla801@gmail.com>
2026-05-20 08:39:58 +0530

    first commit

---
+++ flask_code.py
@@ -0,0 +1,53 @@
+from flask import Flask,request,jsonify,render_template
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+## import lassocv regressor and standard scaler pickle
+lassocv_model=pickle.load(open('lassocv.pkl','rb'))
+standardscaler=pickle.load(open('scaler.pkl','rb'))
+
+
+application=Flask(__name__)
+app=application
+##
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/predictData',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='POST':
+        Temperature = float(request.form.get('Temperature'))
+        RH          = float(request.form.get('RH'))
+        Ws          = float(request.form.get('Ws'))
+        Rain        = float(request.form.get('Rain'))
+        FFMC        = float(request.form.get('FFMC'))
+        DMC         = float(request.form.get('DMC'))
+        DC         = float(request.form.get('DC'))
+        ISI         = float(request.form.get('ISI'))
+        Classes     = float(request.form.get('Classes'))
+        Region      = float(request.form.get('Region'))
+
+        new_data = pd.DataFrame([[
+            Temperature, RH, Ws, Rain, FFMC, DMC, DC, ISI, Classes, Region
+        ]], columns=[
+            'Temperature', 'RH', 'Ws', 'Rain',
+            'FFMC', 'DMC', 'DC', 'ISI',
+            'Classes', 'Region'
+        ])
+
+        new_data_scaled = standardscaler.transform(new_data)
+
+        result = lassocv_model.predict(new_data_scaled)
+
+        return  render_template('home.html', result=result[0])
+    
+    else:
+        return render_template('home.html')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
